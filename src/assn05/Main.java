8c85ec30f383f5f6d1cda85cb798a11f18dc6d57
yangersss <edwardyang609@gmail.com>
2023-10-29 16:19:43 -0400

    assn05 initial commit

---
+++ src/assn05/Main.java
@@ -0,0 +1,88 @@
+package assn05;
+
+public class Main {
+
+    public static void main(String[] args) {
+        testP1();
+        testP2();
+//        testP3();
+        testP4();
+    }
+
+    // test Part 1
+    public static void testP1(){
+        /*
+        Part 1 - Write some tests to convince yourself that your code for Part 1 is working
+         */
+    }
+
+    // test Part 2
+    public static void testP2(){
+       /*
+        Part 2 - Write some tests to convince yourself that your code for Part 2 is working
+         */
+    }
+
+    /*
+    Part 3
+     */
+    public static void testP3(){
+        MaxBinHeapER transfer = new MaxBinHeapER(makePatients());
+        Prioritized[] arr = transfer.getAsArray();
+        for(int i = 0; i < transfer.size(); i++) {
+            System.out.println("Value: " + arr[i].getValue()
+                    + ", Priority: " + arr[i].getPriority());
+        }
+    }
+
+    /*
+    Part 4
+     */
+    public static void testP4() {
+               /*
+        Part 4 - Write some tests to convince yourself that your code for Part 4 is working
+         */
+
+    }
+
+    public static void fillER(MaxBinHeapER complexER) {
+        for(int i = 0; i < 100000; i++) {
+            complexER.enqueue(i);
+        }
+    }
+    public static void fillER(SimpleEmergencyRoom simpleER) {
+        for(int i = 0; i < 100000; i++) {
+            simpleER.addPatient(i);
+        }
+    }
+
+    public static Patient[] makePatients() {
+        Patient[] patients = new Patient[10];
+        for(int i = 0; i < 10; i++) {
+            patients[i] = new Patient(i);
+        }
+        return patients;
+    }
+    
+    public static double[] compareRuntimes() {
+    	// Array which you will populate as part of Part 4
+    	double[] results = new double[4];
+    	
+        SimpleEmergencyRoom simplePQ = new SimpleEmergencyRoom();
+        fillER(simplePQ);
+
+        // Code for (Task 4.1) Here
+
+
+        MaxBinHeapER binHeap = new MaxBinHeapER();
+        fillER(binHeap);
+
+        // Code for (Task 4.2) Here
+
+        return results;
+    }
+
+}
+
+
+
---
+++ src/assn05/MaxBinHeapER.java
@@ -0,0 +1,64 @@
+package assn05;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MaxBinHeapER  <V, P extends Comparable<P>> implements BinaryHeap<V, P> {
+
+    private List<Prioritized<V,P>> _heap;
+    /**
+     * Constructor that creates an empty heap of hospital.Prioritized objects.
+     */
+    public MaxBinHeapER() {
+        _heap = new ArrayList<>();
+    }
+
+    /**
+     * Constructor that builds a heap given an initial array of hospital.Prioritized objects.
+     */
+    // TODO (Part 3): overloaded constructor
+    public MaxBinHeapER(Prioritized<V, P>[] initialEntries ) {
+
+    }
+
+    @Override
+    public int size() {
+        return _heap.size();
+    }
+
+    // TODO: enqueue
+    @Override
+    public void enqueue(V value, P priority) {
+
+    }
+
+    // TODO: enqueue
+    public void enqueue(V value) {
+    }
+
+    // TODO: dequeue
+    @Override
+    public V dequeue() {
+
+        return null;
+    }
+
+    // TODO: getMax
+    @Override
+    public V getMax() {
+       return null;
+    }
+
+    @Override
+    public Prioritized<V, P>[] getAsArray() {
+        Prioritized<V,P>[] result = (Prioritized<V, P>[]) Array.newInstance(Prioritized.class, size());
+        return _heap.toArray(result);
+    }
+
+
+
+
+
+
+}
